6cc008526f829eb69a73552a094f0f618aadf42b
Lucas Lourenço <lucascelestiano@gmail.com>
2024-02-29 14:09:34 -0400

    include google pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito, Roboto } from "next/font/google";
 import StyledComponentsRegistry from "../lib/registry";
 import { Metadata } from "next";
+import Script from "next/script";
+import Image from "next/image";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -44,6 +46,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className={`${roboto.variable} ${nunito.variable}`}>
+      <head>
+        <Script id="meta-pixel">
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3756614457960490');
+            fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=3756614457960490&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
